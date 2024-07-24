c2846d5346ec39d68d39f19c754fca315c78c367
Daniel Ortiz <jdsaquique@gmail.com>
2024-07-24 12:28:19 -0600

    Configuración genereal rockemma

---
+++ js/principal.js
@@ -0,0 +1,23 @@
+let root=document.querySelector("#root");
+
+
+let div_formularios=document.createElement("div");
+div_formularios.className="div_formularios";
+
+let formularios=[{nombre:"login",link:"componenntes/login/login.html"},
+    {nombre:"login",link:"#"},
+    {nombre:"login",link:"#"},
+    {nombre:"login",link:"#"}];
+formularios.forEach(itemlista =>{
+    alert("hola")
+
+
+let item=document.createElement("a");
+
+item.className="item";
+item.href=itemlista.link;
+item.innerText=itemlista.nombre;
+
+div_formularios.appendChild(item);
+});
+root.appendChild(div_formularios);
